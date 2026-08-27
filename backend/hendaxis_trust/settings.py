86dcd63c82ae66aa5b403f438d14229eca952dfe
Dennis D. <deedadey@gmail.com>
2026-08-27 02:20:08 +0000

    bug fixes

--- backend/hendaxis_trust/settings.py
+++ backend/hendaxis_trust/settings.py
@@ -144,7 +144,12 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
